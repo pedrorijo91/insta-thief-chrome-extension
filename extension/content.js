95c37b0f582c73aae883eca8fff749ada2951611
pedrorijo91 <pedrorijo91@gmail.com>
2017-05-06 11:34:48 +0100

    Fix minor issues
    - extract class name to variable
    - prevent exception throw

--- extension/content.js
+++ extension/content.js
@@ -13,9 +13,12 @@ s.onload = function() {
 };
 (document.head || document.documentElement).appendChild(s);
 
+const arrowClassName = '_90kqf';
+const breadcrumbClassName = '_lfres';
+
 function detectMultiPic() {
-	return document.getElementsByClassName('_90kqf').length > 0 || 
-		document.getElementsByClassName('_lfres') > 0;
+	return document.getElementsByClassName(arrowClassName).length > 0 || 
+		document.getElementsByClassName(breadcrumbClassName) > 0;
 }
 
 const tabUrl = window.location.href;
@@ -26,8 +29,13 @@ document.addEventListener('RW759_connectExtension', function(e) {
     const isMultiPic = detectMultiPic();
 
 	if(isMultiPic) {
-		const urls = sharedData.entry_data.PostPage[0].graphql.shortcode_media.edge_sidecar_to_children.edges.map(el => { return el.node.display_url; });
-		chrome.runtime.sendMessage({ "action": ActionEnum.sendMultiPic, "tab_url": tabUrl, "urls": urls});
+		try {
+			const urls = sharedData.entry_data.PostPage[0].graphql.shortcode_media.edge_sidecar_to_children.edges.map(el => { return el.node.display_url; });
+			chrome.runtime.sendMessage({ "action": ActionEnum.sendMultiPic, "tab_url": tabUrl, "urls": urls});
+		} catch (err) {
+			console.error('could not get multipic urls: ' + err);
+		}
+		
 	} else {
 		const pic = document.getElementById("pImage_0");
 		chrome.runtime.sendMessage({ "action": ActionEnum.sendPic, "tab_url": tabUrl, "pic_url": pic.src });
